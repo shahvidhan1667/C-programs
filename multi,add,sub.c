015b4f992c5e2029f8978cd2a057ab97a7dbffd4
shahvidhan1667 <shahvidhan.sp@gmail.com>
2025-08-21 21:42:35 +0530

    Add files via upload

---
+++ multi,add,sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,e;
+printf("Enter 2 numbers:");
+scanf("%d%d",&a,&b);
+c=a*b;
+d=a+b;
+e=a-b;
+printf("Multiplication  will be:%d ",c);
+printf("Addition will be:%d ",d);
+printf("Subraction  will be:%d ",e);
+}
