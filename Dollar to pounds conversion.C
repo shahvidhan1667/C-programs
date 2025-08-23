737ab384fac9e918c3e05da0b5e1df783a7836d7
shahvidhan1667 <shahvidhan.sp@gmail.com>
2025-08-23 22:29:24 +0530

    Add files via upload

---
+++ Dollar to pounds conversion.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter value of Dollars");
+scanf("%d",&a);
+
+b=a*48;
+c=b/70;
+
+printf("Pounds will be equal to:%d",c);
+getch();
+}
